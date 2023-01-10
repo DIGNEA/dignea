4c6af0dcc9a7b92b149772d7df3b254d8bcb21fe
amarrerod <amarrerd@ull.edu.es>
2023-01-10 10:28:33 +0000

    Deploying to gh-pages from  @ 6ad2fa223a796c58c7aeb2d2d95db27a46382b0a 🚀

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -27,64 +27,65 @@ var NAVTREE =
   [ "dignea", "index.html", [
     [ "Getting Started", "md_docs_markdown_00_getting_started.html", [
       [ "Build DIGNEA", "index.html#autotoc_md0", [
-        [ "Dependencies", "index.html#autotoc_md1", null ]
+        [ "Docker Image", "index.html#autotoc_md1", null ],
+        [ "Dependencies", "index.html#autotoc_md2", null ]
       ] ],
-      [ "Publications", "index.html#autotoc_md2", null ],
-      [ "How DIGNEA works.", "md_docs_markdown_00_getting_started.html#autotoc_md4", [
-        [ "Main components.", "md_docs_markdown_00_getting_started.html#autotoc_md5", null ],
-        [ "Results", "md_docs_markdown_00_getting_started.html#autotoc_md6", null ],
-        [ "More information", "md_docs_markdown_00_getting_started.html#autotoc_md7", null ]
+      [ "Publications", "index.html#autotoc_md3", null ],
+      [ "How DIGNEA works.", "md_docs_markdown_00_getting_started.html#autotoc_md5", [
+        [ "Main components.", "md_docs_markdown_00_getting_started.html#autotoc_md6", null ],
+        [ "Results", "md_docs_markdown_00_getting_started.html#autotoc_md7", null ],
+        [ "More information", "md_docs_markdown_00_getting_started.html#autotoc_md8", null ]
       ] ]
     ] ],
     [ "Instance Generation Example", "md_docs_markdown_01_0_mea_example.html", [
-      [ "Generating Knapsack Problem Instances with DIGNEA", "md_docs_markdown_01_0_mea_example.html#autotoc_md9", [
-        [ "Parameter configuration", "md_docs_markdown_01_0_mea_example.html#autotoc_md10", [
-          [ "MEA configuration", "md_docs_markdown_01_0_mea_example.html#autotoc_md11", null ],
-          [ "Knapsack Problem instances", "md_docs_markdown_01_0_mea_example.html#autotoc_md12", null ]
+      [ "Generating Knapsack Problem Instances with DIGNEA", "md_docs_markdown_01_0_mea_example.html#autotoc_md10", [
+        [ "Parameter configuration", "md_docs_markdown_01_0_mea_example.html#autotoc_md11", [
+          [ "MEA configuration", "md_docs_markdown_01_0_mea_example.html#autotoc_md12", null ],
+          [ "Knapsack Problem instances", "md_docs_markdown_01_0_mea_example.html#autotoc_md13", null ]
         ] ],
-        [ "Creating a portfolio of solvers", "md_docs_markdown_01_0_mea_example.html#autotoc_md13", null ],
-        [ "Instantiate a MEA object", "md_docs_markdown_01_0_mea_example.html#autotoc_md14", null ],
-        [ "Run the experiment", "md_docs_markdown_01_0_mea_example.html#autotoc_md15", null ],
-        [ "Example of results", "md_docs_markdown_01_0_mea_example.html#autotoc_md16", null ]
+        [ "Creating a portfolio of solvers", "md_docs_markdown_01_0_mea_example.html#autotoc_md14", null ],
+        [ "Instantiate a MEA object", "md_docs_markdown_01_0_mea_example.html#autotoc_md15", null ],
+        [ "Run the experiment", "md_docs_markdown_01_0_mea_example.html#autotoc_md16", null ],
+        [ "Example of results", "md_docs_markdown_01_0_mea_example.html#autotoc_md17", null ]
       ] ]
     ] ],
     [ "Create an algorithm", "md_docs_markdown_01_create_algorithm.html", [
-      [ "Creating the base Simulated Annealing class.", "md_docs_markdown_01_create_algorithm.html#autotoc_md18", null ],
-      [ "Define the methods in SimulatedAnnealing", "md_docs_markdown_01_create_algorithm.html#autotoc_md19", null ],
-      [ "Define the remaining methods.", "md_docs_markdown_01_create_algorithm.html#autotoc_md20", [
-        [ "Constructor", "md_docs_markdown_01_create_algorithm.html#autotoc_md21", null ],
-        [ "Run", "md_docs_markdown_01_create_algorithm.html#autotoc_md22", null ],
-        [ "Perturbation", "md_docs_markdown_01_create_algorithm.html#autotoc_md23", null ],
-        [ "Population management", "md_docs_markdown_01_create_algorithm.html#autotoc_md24", null ],
-        [ "Progress", "md_docs_markdown_01_create_algorithm.html#autotoc_md25", null ],
-        [ "Getters and Setters", "md_docs_markdown_01_create_algorithm.html#autotoc_md26", null ]
+      [ "Creating the base Simulated Annealing class.", "md_docs_markdown_01_create_algorithm.html#autotoc_md19", null ],
+      [ "Define the methods in SimulatedAnnealing", "md_docs_markdown_01_create_algorithm.html#autotoc_md20", null ],
+      [ "Define the remaining methods.", "md_docs_markdown_01_create_algorithm.html#autotoc_md21", [
+        [ "Constructor", "md_docs_markdown_01_create_algorithm.html#autotoc_md22", null ],
+        [ "Run", "md_docs_markdown_01_create_algorithm.html#autotoc_md23", null ],
+        [ "Perturbation", "md_docs_markdown_01_create_algorithm.html#autotoc_md24", null ],
+        [ "Population management", "md_docs_markdown_01_create_algorithm.html#autotoc_md25", null ],
+        [ "Progress", "md_docs_markdown_01_create_algorithm.html#autotoc_md26", null ],
+        [ "Getters and Setters", "md_docs_markdown_01_create_algorithm.html#autotoc_md27", null ]
       ] ]
     ] ],
     [ "Create a problem", "md_docs_markdown_02_create_problem.html", [
-      [ "Creating the Travelling Salesman Problem (TSP)", "md_docs_markdown_02_create_problem.html#autotoc_md28", [
-        [ "Defining the methods and attributes for TSP", "md_docs_markdown_02_create_problem.html#autotoc_md29", null ],
-        [ "TSP implementation in TSP.cpp", "md_docs_markdown_02_create_problem.html#autotoc_md30", [
-          [ "Constructors", "md_docs_markdown_02_create_problem.html#autotoc_md31", null ],
-          [ "Auxiliary building methods", "md_docs_markdown_02_create_problem.html#autotoc_md32", null ],
-          [ "Create new random solutions", "md_docs_markdown_02_create_problem.html#autotoc_md33", null ],
-          [ "Evaluation", "md_docs_markdown_02_create_problem.html#autotoc_md34", null ],
-          [ "Getters", "md_docs_markdown_02_create_problem.html#autotoc_md35", null ],
-          [ "JSON", "md_docs_markdown_02_create_problem.html#autotoc_md36", null ]
+      [ "Creating the Travelling Salesman Problem (TSP)", "md_docs_markdown_02_create_problem.html#autotoc_md29", [
+        [ "Defining the methods and attributes for TSP", "md_docs_markdown_02_create_problem.html#autotoc_md30", null ],
+        [ "TSP implementation in TSP.cpp", "md_docs_markdown_02_create_problem.html#autotoc_md31", [
+          [ "Constructors", "md_docs_markdown_02_create_problem.html#autotoc_md32", null ],
+          [ "Auxiliary building methods", "md_docs_markdown_02_create_problem.html#autotoc_md33", null ],
+          [ "Create new random solutions", "md_docs_markdown_02_create_problem.html#autotoc_md34", null ],
+          [ "Evaluation", "md_docs_markdown_02_create_problem.html#autotoc_md35", null ],
+          [ "Getters", "md_docs_markdown_02_create_problem.html#autotoc_md36", null ],
+          [ "JSON", "md_docs_markdown_02_create_problem.html#autotoc_md37", null ]
         ] ]
       ] ]
     ] ],
     [ "Create a instance generation problem", "md_docs_markdown_03_create_instance_generator.html", [
-      [ "Creating the Travelling Salesman Problem (TSP)", "md_docs_markdown_03_create_instance_generator.html#autotoc_md38", null ],
-      [ "Create the new MEAProblem subclass: ITSPProblem class.", "md_docs_markdown_03_create_instance_generator.html#autotoc_md39", [
-        [ "Constructors", "md_docs_markdown_03_create_instance_generator.html#autotoc_md40", null ],
-        [ "Generate Optimization Problem from Instance Solution", "md_docs_markdown_03_create_instance_generator.html#autotoc_md41", null ],
-        [ "Create new solutions", "md_docs_markdown_03_create_instance_generator.html#autotoc_md42", null ],
-        [ "Evaluation and feature calculation", "md_docs_markdown_03_create_instance_generator.html#autotoc_md43", null ],
-        [ "Getters and Setters", "md_docs_markdown_03_create_instance_generator.html#autotoc_md44", null ]
+      [ "Creating the Travelling Salesman Problem (TSP)", "md_docs_markdown_03_create_instance_generator.html#autotoc_md39", null ],
+      [ "Create the new MEAProblem subclass: ITSPProblem class.", "md_docs_markdown_03_create_instance_generator.html#autotoc_md40", [
+        [ "Constructors", "md_docs_markdown_03_create_instance_generator.html#autotoc_md41", null ],
+        [ "Generate Optimization Problem from Instance Solution", "md_docs_markdown_03_create_instance_generator.html#autotoc_md42", null ],
+        [ "Create new solutions", "md_docs_markdown_03_create_instance_generator.html#autotoc_md43", null ],
+        [ "Evaluation and feature calculation", "md_docs_markdown_03_create_instance_generator.html#autotoc_md44", null ],
+        [ "Getters and Setters", "md_docs_markdown_03_create_instance_generator.html#autotoc_md45", null ]
       ] ],
-      [ "Create the solution for the instance generation problem: ITSPSolution class.", "md_docs_markdown_03_create_instance_generator.html#autotoc_md45", [
-        [ "Constructors", "md_docs_markdown_03_create_instance_generator.html#autotoc_md46", null ],
-        [ "Representation", "md_docs_markdown_03_create_instance_generator.html#autotoc_md47", null ]
+      [ "Create the solution for the instance generation problem: ITSPSolution class.", "md_docs_markdown_03_create_instance_generator.html#autotoc_md46", [
+        [ "Constructors", "md_docs_markdown_03_create_instance_generator.html#autotoc_md47", null ],
+        [ "Representation", "md_docs_markdown_03_create_instance_generator.html#autotoc_md48", null ]
       ] ]
     ] ],
     [ "Deprecated List", "deprecated.html", null ],
